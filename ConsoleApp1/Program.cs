def0b26d5df3b5a73995d50efeb0a0fc25c208a1
abdullapro2006 <abdullahmanafli38@gmail.com>
2023-06-24 15:54:37 +0400

    code created

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+
+namespace AboutPeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Teacher teacher = new Teacher("Aziza","Suleymanova",27);
+            Student student = new Student("Abdullah","Manafli",16);
+            teacher.Explain();
+            student.Learn();
+            
+            
+            
+            
+        }
+
+
+
+    }
+
+
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            
+
+        }
+        
+    }
+
+    public class Teacher : Person
+    {
+
+        public  Teacher(string name, string surname,int age) : base(name, surname, age)
+        {
+           
+
+        }
+
+        public void Explain()
+        {
+            Console.WriteLine("Explaing...");
+        }
+    }
+      
+       public class Student : Teacher
+    {     
+         public Student(string name, string surname,int age) : base (name, surname, age)
+        {
+
+        }
+         public void Learn()
+        {
+            Console.WriteLine("Learning...");
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
